8c0bb8fcc77f0f9fafd844596d6a172499c60e20
Radosław Grzanka <radoslawg@gmail.com>
2025-01-31 22:28:51 +0000

    Random bool with random weight

--- src/bin/random.rs
+++ src/bin/random.rs
@@ -4,4 +4,8 @@ fn main() {
     let mut rng = rand::rng();
     let random_number: u32 = rng.random_range(..11);
     println!("Random number: {}", random_number);
+    
+    let random_weight: f64 = rng.random();
+    let random_bool = rng.random_bool(random_weight);
+    println!("Random bool: {} with Random weight: {}", random_bool, random_weight);
 }
